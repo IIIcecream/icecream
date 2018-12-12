4c5066bcd5294b2af018b0fd68a7bf38710a51ea
tangw-b <tangw-b@glodon.com>
2018-12-12 17:55:24 +0800

    链表

---
+++ algorithm/acm/include/linkedList/MergeTwoSortedLists.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "IACM.h"
+#include "LinkedList\LinkedListCommon.h"
+
+class Merge2SortedLists : public IACM
+{
+public:
+    Merge2SortedLists(ListNode* l1, ListNode* l2) : m_pList1(l1), m_pList2(l2) {}
+    virtual bool solve() override;
+
+private:
+    ListNode *m_pList1, *m_pList2;
+};
---
+++ algorithm/acm/src/linkedList/MergeTwoSortedLists.cpp
@@ -0,0 +1,8 @@
+#include "linkedList\MergeTwoSortedLists.h"
+
+bool Merge2SortedLists::solve()
+{
+    if (!m_pList1 || !m_pList2) return false;
+    return true;
+}
+
