b4caeb30caf7c700278fb766cc1e637f99037d06
tangw-b <tangw-b@glodon.com>
2019-03-15 10:14:12 +0800

    迭代器模式

--- c++/DesignPatterns/BehaviorType/Iterator/include/Iterator.h
+++ c++/DesignPatterns/BehaviorType/Iterator/include/Iterator.h
@@ -1,2 +1,41 @@
 #pragma once
 
+/*!
+*@brief        µü´úÆ÷Ä£Ê½
+*@author       tangw-b  2019/03/14  17:33
+*/
+
+#include <string>
+using namespace std;
+
+template<class Item>
+class Iterator
+{
+public:
+    Iterator() {}
+    virtual ~Iterator() {}
+    virtual Item first() = 0;
+    virtual Item next() = 0;
+    virtual Item cur() = 0;
+    virtual bool isDone() = 0;
+};
+
+template<class Item>
+class Aggregate;
+
+template<class Item>
+class ConcreteIterator : public Iterator<Item>
+{
+public:
+    ConcreteIterator(Aggregate<Item>* pAggregate);
+    virtual ~ConcreteIterator();
+
+    virtual Item first() override;
+    virtual Item Next() override;
+    virtual Item GetCur() override;
+    virtual bool IsDone() override;
+
+private:
+    Aggregate *m_pAggregate;
+    int m_nCur;
+};
---
+++ c++/DesignPatterns/BehaviorType/Iterator/src/Iterator.cpp
@@ -0,0 +1,39 @@
+#include "Iterator.h"
+#include "Aggregate.h"
+
+template<class Item>
+ConcreteIterator<Item>::ConcreteIterator(Aggregate<Item>* pAggregate)
+    : m_pAggregate(pAggregate), m_nCur(0)
+{
+
+}
+
+template<class Item>
+ConcreteIterator<Item>::~ConcreteIterator()
+{
+    m_pAggregate = nullptr;
+}
+
+template<class Item>
+Item ConcreteIterator<Item>::first()
+{
+    return m_pAggregate->getItem(0);
+}
+
+template<class Item>
+Item ConcreteIterator<Item>::Next()
+{
+    return m_pAggregate->getItem(++m_nCur);
+}
+
+template<class Item>
+Item ConcreteIterator<Item>::GetCur()
+{
+    return m_pAggregate->getItem(m_nCur);
+}
+
+template<class Item>
+bool ConcreteIterator<Item>::IsDone()
+{
+    return m_nCur >= m_pAggregate.Count();
+}
