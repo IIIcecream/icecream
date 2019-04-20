be7bf62cf5f6f2f97ff82785b407643100d36607
tangwei147* <981091829@qq.com>
2019-04-20 21:39:21 +0800

    easy socket server client commit

---
+++ c++/socket/HelloSocket/HelloSocket.cpp
@@ -0,0 +1,27 @@
+﻿#include <winsock.h>
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// 启动windows socket 2.2环境
+	WORD ver = MAKEWORD(2, 2);
+	WSADATA data;
+	WSAStartup(ver, &data);
+
+	//////////////////////////////////////////////////////////////////////////
+	// 用socketAPI建立简易TCP客户端
+	// 1 建立一个socket
+
+	// 2 链接服务器connect
+	// 3 接收服务器recv
+	// 4 关闭套接字closesocket
+
+
+
+	// 清除windows socket环境
+	WSACleanup();
+
+	return 0;
+}
