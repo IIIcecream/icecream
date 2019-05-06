abd55e0a5d46b14e3581d21ecb95bd1ecb7d0e97
tangw-b <tangw-b@glodon.com>
2019-05-06 19:52:20 +0800

    LeetCode 221 Maximal Square

---
+++ algorithm/acm/include/dynamicProgramming/MaximalSquare.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "IACM.h"
+#include <vector>
+using namespace std;
+
+// LeetCode T221 最大正方形
+class MaximalSquare : public IACM
+{
+public:
+    MaximalSquare(vector<vector<char>>& matrix) : m_matrix(matrix) {}
+    virtual bool solve() override;
+
+private:
+    int maximalSquare(vector<vector<char>>& matrix);
+
+private:
+    vector<vector<char>> m_matrix;
+};
